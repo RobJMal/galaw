76351807f417c09cf7b053dbe8b202633750c649
RobJMal <robjmal@gmail.com>
2026-07-08 01:06:06 +0200

    refactor: moving parse_vec3_str to utils.rs

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,8 @@
 use std::fs;
 
+use taligalaw::utils::parse_vec3_str;
 use taligalaw::types::{EulerRPY, Joint, Position3D, Transform};
 
-fn parse_vec3_str(input_str: &str) -> Result<(f64, f64, f64), Box<dyn std::error::Error>> {
-    // Parses and extracts values from string. Assumes will contain 3 values.
-    let vals: Vec<f64> = input_str
-        .split_whitespace()
-        .map(|n| n.parse::<f64>())
-        .collect::<Result<Vec<f64>, _>>()?;
-
-    if vals.len() != 3 {
-        return Err("expected exactly 3 values".into());
-    }
-
-    Ok((vals[0], vals[1], vals[2]))
-}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let content: String = fs::read_to_string("assets/simple_robot.urdf")?;
