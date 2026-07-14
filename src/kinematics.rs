96be7c5aefe0758c4df344057036a91e23c3789e
Robert Jomar Malate <robjmal@gmail.com>
2026-07-15 01:56:08 +0200

    chore: ran cargo fmt

--- src/kinematics.rs
+++ src/kinematics.rs
@@ -1,47 +1,56 @@
-use std::{collections::HashMap};
+use std::collections::HashMap;
 
 use crate::types::{Link, Position3D, Quaternion, RobotModel, Transform};
 
 impl RobotModel {
-    pub fn compute_fk(&self, angles: &[f64]) -> Result<HashMap<Link, Transform>, Box<dyn std::error::Error>> {
+    pub fn compute_fk(
+        &self,
+        angles: &[f64],
+    ) -> Result<HashMap<Link, Transform>, Box<dyn std::error::Error>> {
         /* Computes forward kinematics of a model */
         if angles.len() != self.joints.len() {
             return Err(format!(
                 "expected {} angles, got {}",
                 self.joints.len(),
                 angles.len()
-            ).into());
+            )
+            .into());
         }
 
         let mut links: HashMap<Link, Transform> = HashMap::new();
-        let base_link_transform: Transform = Transform { 
-            position: Position3D { x: 0.0, y: 0.0, z: 0.0 }, 
-            orientation: Quaternion::identity(), 
+        let base_link_transform: Transform = Transform {
+            position: Position3D {
+                x: 0.0,
+                y: 0.0,
+                z: 0.0,
+            },
+            orientation: Quaternion::identity(),
         };
         links.insert(self.links[0].clone(), base_link_transform);
-        
+
         for (i, joint) in self.joints.iter().enumerate() {
-            // Extracting info about the joint command 
+            // Extracting info about the joint command
             let joint_axis = &joint.axis;
-            let joint_axis_norm: f64 = f64::sqrt(joint_axis.x.powi(2) + joint_axis.y.powi(2) + joint_axis.z.powi(2));
-            let joint_axis_normalized = Position3D { 
+            let joint_axis_norm: f64 =
+                f64::sqrt(joint_axis.x.powi(2) + joint_axis.y.powi(2) + joint_axis.z.powi(2));
+            let joint_axis_normalized = Position3D {
                 x: joint_axis.x / joint_axis_norm,
                 y: joint_axis.y / joint_axis_norm,
                 z: joint_axis.z / joint_axis_norm,
-             };
+            };
 
-             let quat_joint: Quaternion = Quaternion { 
-                x: joint_axis_normalized.x * f64::sin(angles[i] / 2.0), 
-                y: joint_axis_normalized.y * f64::sin(angles[i] / 2.0), 
-                z: joint_axis_normalized.z * f64::sin(angles[i] / 2.0), 
-                w: f64::cos(angles[i] / 2.0), 
+            let quat_joint: Quaternion = Quaternion {
+                x: joint_axis_normalized.x * f64::sin(angles[i] / 2.0),
+                y: joint_axis_normalized.y * f64::sin(angles[i] / 2.0),
+                z: joint_axis_normalized.z * f64::sin(angles[i] / 2.0),
+                w: f64::cos(angles[i] / 2.0),
             };
 
             let quat_local: Quaternion = joint.transform.orientation.multiply(&quat_joint);
-            
+
             let link_prev_quat = links[&self.links[i]].orientation;
-            let link_n_quat = link_prev_quat.multiply(&quat_local); 
-            
+            let link_n_quat = link_prev_quat.multiply(&quat_local);
+
             let quat_joint_translation = Quaternion {
                 x: joint.transform.position.x,
                 y: joint.transform.position.y,
@@ -49,11 +58,22 @@ impl RobotModel {
                 w: 0.0,
             };
 
-            let rot_transform = link_prev_quat.multiply(&quat_joint_translation.multiply(&link_prev_quat.inverse()));
-            let rot_position = Position3D {x: rot_transform.x, y: rot_transform.y, z: rot_transform.z};
+            let rot_transform = link_prev_quat
+                .multiply(&quat_joint_translation.multiply(&link_prev_quat.inverse()));
+            let rot_position = Position3D {
+                x: rot_transform.x,
+                y: rot_transform.y,
+                z: rot_transform.z,
+            };
             let link_n_position = links[&self.links[i]].position + rot_position;
 
-            links.insert(self.links[i+1].clone(), Transform { position: link_n_position, orientation: link_n_quat });
+            links.insert(
+                self.links[i + 1].clone(),
+                Transform {
+                    position: link_n_position,
+                    orientation: link_n_quat,
+                },
+            );
         }
 
         Ok(links)
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use taligalaw::load_urdf;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let robot_model =  load_urdf("assets/simple_robot.urdf")?;
-    
+    let robot_model = load_urdf("assets/simple_robot.urdf")?;
+
     println!("robot name: {:?}", robot_model.name);
     println!("number of links: {:?}", robot_model.links.len());
     println!("number of joints: {:?}", robot_model.joints.len());
@@ -15,7 +15,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         println!("Joint: {:?}", joint.name);
     }
 
-    let joint_cmds= [0.0, 0.0, 0.0];
+    let joint_cmds = [0.0, 0.0, 0.0];
     match robot_model.compute_fk(&joint_cmds) {
         Ok(links) => println!("links: {:?}", links),
         Err(e) => eprintln!("Error: {}", e),
