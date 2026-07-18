057f0f08344cd4021b25fd3f38dfdd1c4e1372c5
Robert Jomar Malate <robjmal@gmail.com>
2026-07-18 14:53:04 +0200

    refactor: creating helper methods to parse link and joints

--- src/parser.rs
+++ src/parser.rs
@@ -8,6 +8,98 @@ use nalgebra::{Isometry3, Translation3, Unit, UnitQuaternion, Vector3};
 use crate::types::{GalawModel, Joint, Link};
 use crate::utils::parse_vec3_str;
 
+
+/// Parses <link> tag into a `Link`
+fn parse_link(node: roxmltree::Node<'_, '_>) -> Result<Link, Box<dyn std::error::Error>> {
+    let link_name: String = node
+        .attribute("name")
+        .ok_or("link missing name attribute")?
+        .to_string(); 
+    Ok(Link {name: link_name})
+}
+
+
+/// Parses <joint> tag into a `Joint`
+fn parse_joint(node: roxmltree::Node<'_, '_>) -> Result<Joint, Box<dyn std::error::Error>> {
+    let name: String = node
+        .attribute("name")
+        .ok_or("joint missing name attribute")?
+        .to_string();
+    let parent: String = node
+        .children()
+        .find(|n| n.tag_name().name() == "parent")
+        .and_then(|n| n.attribute("link"))
+        .ok_or_else(|| format!("missing parent link for joint {}", name))?
+        .to_string();
+    let child: String = node
+        .children()
+        .find(|n| n.tag_name().name() == "child")
+        .and_then(|n| n.attribute("link"))
+        .ok_or_else(|| format!("missing child link for joint {}", name))?
+        .to_string();
+
+    // Extracting joint XYZ and RPY
+    let joint_origin = node
+        .children()
+        .find(|n| n.tag_name().name() == "origin")
+        .ok_or_else(|| format!("missing origin for joint {}", name))?;
+
+    let xyz_str: &str = joint_origin
+        .attribute("xyz")
+        .ok_or_else(|| format!("missing xyz for joint {}", name))?;
+    let (x, y, z) = parse_vec3_str(xyz_str)?;
+    let xyz = Vector3::new(x, y, z);
+
+    let rpy_str = joint_origin
+        .attribute("rpy")
+        .ok_or_else(|| format!("missing rpy for joint {}", name))?;
+    let (roll, pitch, yaw) = parse_vec3_str(rpy_str)?;
+    let rotation = UnitQuaternion::from_euler_angles(roll, pitch, yaw);
+
+    let transform = Isometry3::from_parts(Translation3::from(xyz), rotation);
+
+    // Extracting axis angles
+    let axis_str: &str = node
+        .children()
+        .find(|n| n.tag_name().name() == "axis")
+        .and_then(|n| n.attribute("xyz"))
+        .ok_or_else(|| format!("missing axis xyz value for joint {}", name))?;
+    let (axis_x, axis_y, axis_z) = parse_vec3_str(axis_str)?;
+    let axis = Unit::new_normalize(Vector3::new(axis_x, axis_y, axis_z));
+
+    // Extracting joint limits
+    let joint_limit = node
+        .children()
+        .find(|n| n.tag_name().name() == "limit")
+        .ok_or_else(|| format!("missing joint limits for joint {}", name))?;
+
+    let limit_lower: f64 = joint_limit
+        .attribute("lower")
+        .ok_or_else(|| format!("missing joint limit lower for joint {}", name))?
+        .parse::<f64>()?;
+    let limit_upper: f64 = joint_limit
+        .attribute("upper")
+        .ok_or_else(|| format!("missing joint limit upper for joint {}", name))?
+        .parse::<f64>()?;
+
+    // Creating joint
+    let joint: Joint = Joint {
+        name,
+        parent,
+        parent_link_idx: 0, // Dummy as it will be handled in next lines
+        child,
+        child_link_idx: 0, // Dummy as it will be handled in next lines
+        transform,
+        axis,
+        limit_lower,
+        limit_upper,
+        cmd_idx: 0,     // Resolved later
+    };
+
+    Ok(joint)
+}
+
+
 /// Parses a URDF file into a `GalawModel`.
 ///
 /// After XML parsing, it resolves the joint order via Breadth-First Search (BFS)
@@ -26,86 +118,11 @@ pub fn load_urdf(urdf_path: &str) -> Result<GalawModel, Box<dyn std::error::Erro
 
     for node in doc.descendants() {
         if node.tag_name().name() == "link" {
-            let link_name: String = node
-                .attribute("name")
-                .ok_or("link missing name attribute")?
-                .to_string();
-            links.push(Link { name: link_name });
+            let link = parse_link(node)?;
+            links.push(link);
         } else if node.tag_name().name() == "joint" {
-            let name: String = node
-                .attribute("name")
-                .ok_or("joint missing name attribute")?
-                .to_string();
-            let parent: String = node
-                .children()
-                .find(|n| n.tag_name().name() == "parent")
-                .and_then(|n| n.attribute("link"))
-                .ok_or_else(|| format!("missing parent link for joint {}", name))?
-                .to_string();
-            let child: String = node
-                .children()
-                .find(|n| n.tag_name().name() == "child")
-                .and_then(|n| n.attribute("link"))
-                .ok_or_else(|| format!("missing child link for joint {}", name))?
-                .to_string();
-
-            // Extracting joint XYZ and RPY
-            let joint_origin = node
-                .children()
-                .find(|n| n.tag_name().name() == "origin")
-                .ok_or_else(|| format!("missing origin for joint {}", name))?;
-
-            let xyz_str: &str = joint_origin
-                .attribute("xyz")
-                .ok_or_else(|| format!("missing xyz for joint {}", name))?;
-            let (x, y, z) = parse_vec3_str(xyz_str)?;
-            let xyz = Vector3::new(x, y, z);
-
-            let rpy_str = joint_origin
-                .attribute("rpy")
-                .ok_or_else(|| format!("missing rpy for joint {}", name))?;
-            let (roll, pitch, yaw) = parse_vec3_str(rpy_str)?;
-            let rotation = UnitQuaternion::from_euler_angles(roll, pitch, yaw);
-
-            let transform = Isometry3::from_parts(Translation3::from(xyz), rotation);
-
-            // Extracting axis angles
-            let axis_str: &str = node
-                .children()
-                .find(|n| n.tag_name().name() == "axis")
-                .and_then(|n| n.attribute("xyz"))
-                .ok_or_else(|| format!("missing axis xyz value for joint {}", name))?;
-            let (axis_x, axis_y, axis_z) = parse_vec3_str(axis_str)?;
-            let axis = Unit::new_normalize(Vector3::new(axis_x, axis_y, axis_z));
-
-            // Extracting joint limits
-            let joint_limit = node
-                .children()
-                .find(|n| n.tag_name().name() == "limit")
-                .ok_or_else(|| format!("missing joint limits for joint {}", name))?;
-
-            let limit_lower: f64 = joint_limit
-                .attribute("lower")
-                .ok_or_else(|| format!("missing joint limit lower for joint {}", name))?
-                .parse::<f64>()?;
-            let limit_upper: f64 = joint_limit
-                .attribute("upper")
-                .ok_or_else(|| format!("missing joint limit upper for joint {}", name))?
-                .parse::<f64>()?;
-
-            // Creating joint
-            let joint: Joint = Joint {
-                name,
-                parent,
-                parent_link_idx: 0, // Dummy as it will be handled in next lines
-                child,
-                child_link_idx: 1, // Dummy as it will be handled in next lines
-                transform,
-                axis,
-                limit_lower,
-                limit_upper,
-                cmd_idx: joints.len(),
-            };
+            let mut joint = parse_joint(node)?;
+            joint.cmd_idx = joints.len();
             joints.push(joint);
         }
     }
