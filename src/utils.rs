adb576af8f18cb01a92bd886c6af318d5240ecd7
Robert Jomar Malate <robjmal@gmail.com>
2026-07-27 15:23:55 +0200

    doc: adding docstrings for error, lib, and utils

--- src/utils.rs
+++ src/utils.rs
@@ -1,8 +1,10 @@
 // Custom
 use crate::error::UrdfParseError;
 
+/// Parses and extracts values from a string
+/// 
+/// Assumes that it contains 3 numeric values, raises errors otheriwse.
 pub fn parse_vec3_str(input_str: &str) -> Result<(f64, f64, f64), UrdfParseError> {
-    // Parses and extracts values from string. Assumes will contain 3 values.
     let vals: Vec<f64> = input_str
         .split_whitespace()
         .map(|n| {
