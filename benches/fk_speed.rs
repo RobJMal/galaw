67ec6539124b002821392faaa475b81caf346d46
Robert Jomar Malate <robjmal@gmail.com>
2026-09-21 16:51:09 +0200

    chore: bench cleanup (typos and comments)

--- benches/fk_speed.rs
+++ benches/fk_speed.rs
@@ -118,7 +118,6 @@ fn bench_fk(c: &mut Criterion) {
                         (Some(lower), Some(upper)) => rng.random_range(lower..upper),
                         _ => rng.random_range(0.0..0.0),
                     })
-                    // .map(|j| rng.random_range(j.limit_lower..j.limit_upper))
                     .collect()
             })
             .collect();
